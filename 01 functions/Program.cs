c440559d455f41d8f383523c33a076d7361f16d5
Dajo Hofstee <40821@ma-web.nl>
2025-11-18 13:09:55 +0100

    opdr3

--- 01 functions/Program.cs
+++ 01 functions/Program.cs
@@ -10,7 +10,28 @@
         }
         internal void Run()
         {
+            Console.WriteLine("Hello");
+
+            Console.WriteLine("do you know Tom Clancy the Division?");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+
+            Console.WriteLine("would you survive in Tom Clanys the Division?");
+            string antwoord2 = Console.ReadLine();
+            Console.WriteLine(antwoord2);
+
+            Console.WriteLine("Do you like pasta?");
+            string antwoord3 = Console.ReadLine();
+            Console.WriteLine(antwoord3);
+
+            Console.WriteLine("Do you like shooters games?");
+            string antwoord4 = Console.ReadLine();
+            Console.WriteLine(antwoord4);
+
+            Console.WriteLine("do you like water?");
+            string antwoord5 = Console.ReadLine();
+            Console.WriteLine(antwoord5);
         }
-}
+    }
 
 }
--- 01 functions/obj/Debug/net9.0/01 functions.AssemblyInfo.cs
+++ 01 functions/obj/Debug/net9.0/01 functions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01 functions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414c5ce2ddb7d51c125ad9ff29902b2b4df79d1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353533d99c5483f1c361f28e25a5489abd85b048")]
 [assembly: System.Reflection.AssemblyProductAttribute("01 functions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01 functions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
